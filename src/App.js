e5262e2d582046e0b2df8ad5b251e936fb0b241e
Luca Huettner <luca.huettner@gmail.com>
2018-03-07 15:02:52 +0100

    feat: updated links to use react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Books from './Books';
 import Search from './Search';
 import * as BooksAPI from './BooksAPI';
@@ -27,28 +28,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className='app'>
-        {this.state.showSearchPage ? (
-          <div className='search-books'>
-            <div className='search-books-bar'>
-              <a className='close-search' onClick={() => this.setState({showSearchPage: false})}>Close</a>
-              <div className='search-books-input-wrapper'>
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type='text' placeholder='Search by title or author'/>
-
-              </div>
-            </div>
-            <div className='search-books-results'>
-              <ol className='books-grid'></ol>
-            </div>
-          </div>
-        ) : (
+        <Route path='/search' render={() => (
+            <Search/>
+          )}/>
+        <Route exact path='/' render={() => (
           <div className='list-books'>
             <div className='list-books-title'>
               <h1>MyReads</h1>
@@ -76,10 +59,10 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className='open-search'>
-              <a onClick={() => this.setState({showSearchPage: true})}>Add a book</a>
+              <Link to='/search'>Add a book</Link>
             </div>
           </div>
-        )}
+        )}/>
       </div>
     );
   }
