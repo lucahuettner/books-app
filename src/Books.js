41fcd4bf8bd465ab17af5e7b320fcbb7127a5f17
Luca Huettner <luca.huettner@gmail.com>
2018-03-10 18:38:07 +0100

    fix: books are displaying even without thumbnail

--- src/Books.js
+++ src/Books.js
@@ -3,16 +3,17 @@ import React, { Component } from 'react';
 class Books extends Component {
   render() {
     const {updateShelf, books} = this.props;
+    books.forEach((book) => book.imageLinks && book.imageLinks.thumbnail ? book.imageURL = book.imageLinks.thumbnail : book.imageURL = 'http://via.placeholder.com/128x193?text=No+Cover');
     return (
       <ol className='books-grid'>
         {books.map((book) => (
           <li key={book.id}>
             <div className='book'>
               <div className='book-top'>
-                <div className='book-cover' style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                <div className='book-cover' style={{width: 128, height: 193, backgroundImage: `url(${book.imageURL})`}}></div>
                 <div className='book-shelf-changer'>
                   {/*set the current shelf as the default value of the dropdown menu*/}
-                  <select defaultValue={book.shelf} onChange={(e) => updateShelf(book, e.target.value)}>
+                  <select value={book.shelf} onChange={(e) => updateShelf(book, e.target.value)}>
                     <option value='none' disabled>Move to...</option>
                     <option value='currentlyReading'>Currently Reading</option>
                     <option value='wantToRead'>Want to Read</option>
